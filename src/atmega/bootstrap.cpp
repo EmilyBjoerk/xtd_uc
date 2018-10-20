fde62095946043307a2c869cda22b3ecdc1d1d6b
Emily Björk <EmilyBjoerk@users.noreply.github.com>
2018-10-20 17:13:55 +0200

    Improve boot behavior and use new function names for wdt.

--- src/atmega/bootstrap.cpp
+++ src/atmega/bootstrap.cpp
@@ -1,36 +1,33 @@
 #include "xtd_uc/common.hpp"
 
-#include "xtd_uc/bootstrap.hpp"
 #include "xtd_uc/adc.hpp"
+#include "xtd_uc/bootstrap.hpp"
 #include "xtd_uc/wdt.hpp"
 
 namespace xtd {
   reset_cause bootstrap(bool enable_wdt) {
     reset_cause rst_cause = reset_cause::power_on;
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
-      watchdog::reset_timeout();
+      wdt_reset_timeout();
       rst_cause = static_cast<reset_cause>(MCUSR);
 
-      if (enable_wdt) {
-        WDTCSR |= _BV(WDCE);
-        WDTCSR |= _BV(WDE);
+      // If MCUSR has WDRF set, it must be cleared before changing WDE flag.
+      if ( enable_wdt) {
+        wdt_enable(wdt_timeout::_16ms, false, true);
+        MCUSR = 0;
       } else {
-        WDTCSR |= _BV(WDCE);
-        clr_bit(WDTCSR, WDE);
+	MCUSR = 0;
+	wdt_disable();
       }
-
-      // The WDRF flag in MCUSR forces WDT on (so it remains on after a reset by WDT).
-      // If we desire to not have WDT on, we must be sure to clear this flag. If we do
-      // want WDT on, it will have been set earlier and we should clear this flag anyway.
-      MCUSR = 0;
-
-      // Enable all power reduction
-      PRR = 0xff;
 
       adc_disable();
 
-      watchdog::reset_timeout();
+      // Enable all power reduction
+      PRR = 0xff;
+      ACSR = _BV(ACD); // Disable analog comparator
+
+      wdt_reset_timeout();
     }
     return rst_cause;
   }
-}
+}  // namespace xtd
