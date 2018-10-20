c957cf877d1d59b5f8b876aff40125a861052c81
Emily Björk <EmilyBjoerk@users.noreply.github.com>
2018-10-20 17:13:05 +0200

    Handling of the WDT configuration wasn't correct. Also fix naming and style to match new style.

--- include/xtd_uc/wdt.hpp
+++ include/xtd_uc/wdt.hpp
@@ -9,49 +9,38 @@
 #include "utility.hpp"
 
 namespace xtd {
-  namespace watchdog {
-    enum class timeout : uint8_t {
-      _16ms = 0b000000,
-      _32ms = 0b000001,
-      _64ms = 0b000010,
-      _125ms = 0b000011,
-      _250ms = 0b000100,
-      _500ms = 0b000101,
-      _1000ms = 0b000110,
-      _2000ms = 0b000111,
-      _4000ms = 0b100000,
-      _8000ms = 0b100001
-    };
+  enum class wdt_timeout : uint8_t {
+    _16ms = 0b000000,
+    _32ms = 0b000001,
+    _64ms = 0b000010,
+    _125ms = 0b000011,
+    _250ms = 0b000100,
+    _500ms = 0b000101,
+    _1000ms = 0b000110,
+    _2000ms = 0b000111,
+    _4000ms = 0b100000,
+    _8000ms = 0b100001
+  };
 
-    inline void reset_timeout() { wdt_reset(); }
-    inline void set_timeout(timeout to) {
-      ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        reset_timeout();
-        set_bit(WDTCSR, WDCE);
-        WDTCSR |= static_cast<uint8_t>(to);
-      }
-    }
+  inline void wdt_reset_timeout() { wdt_reset(); }
 
-    inline void enable_irq() { set_bit(WDTCSR, WDIE); }
-    inline void disable_irq() { clr_bit(WDTCSR, WDIE); }
+  inline void wdt_enable_irq() { set_bit(WDTCSR, WDIE); }
+  inline void wdt_disable_irq() { clr_bit(WDTCSR, WDIE); }
 
-    inline bool reset_enabled() { return WDTCSR & _BV(WDE); }
+  inline bool wdt_reset_enabled() { return WDTCSR & _BV(WDE); }
 
-    inline void enable_reset() {
-      ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        reset_timeout();
-        WDTCSR |= _BV(WDCE);
-        WDTCSR |= _BV(WDE);
-      }
-    }
-
-    inline void disable_reset() {
-      ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-        reset_timeout();
-        WDTCSR |= _BV(WDCE);
-        clr_bit(WDTCSR, WDE);
-      }
+  inline void wdt_enable(wdt_timeout timeout, bool irq, bool reset) {
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      wdt_reset_timeout();
+      uint8_t new_value = (irq ? _BV(WDIE) : 0) | (reset ? _BV(WDE) : 0) | static_cast<uint8_t>(timeout);
+      WDTCSR |= _BV(WDCE) | _BV(WDE);
+      WDTCSR = new_value;
     }
   }
-}
+
+  inline void wdt_disable() {
+    wdt_enable(wdt_timeout::_16ms, false, false);
+  }
+}  // namespace xtd
+
 #endif
